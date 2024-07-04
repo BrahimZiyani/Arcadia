6b843b2887e5d32bbdb184b729bf3c638bc55a5b
Brahim Ziyani <brahimziyani@live.fr>
2024-07-04 16:47:29 +0200

    Création du controller PageController et de quelques vues du site ( Nos Habitats et Nos Services )

---
+++ src/Controller/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/habitats', name: 'app_habitats')]
+    public function habitats(): Response
+    {
+        return $this->render('page/habitats.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/services', name: 'app_services')]
+    public function services(): Response
+    {
+        return $this->render('page/services.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+}
