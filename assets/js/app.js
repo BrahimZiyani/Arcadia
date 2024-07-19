2ec8f6657e662bdb250aec1bc64c9ed2fd297efe
BrahimZiyani <brahimziyani@live.fr>
2024-07-19 22:15:09 +0200

    Ajout du Swiper React pour l'accueil

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,19 @@
 // assets/js/app.js
 import React from 'react';
+import ReactDOM from 'react-dom';
 import '../styles/app.css';
-import { createRoot } from 'react-dom/client'; // Importation de createRoot depuis react-dom/client
-import HelloWorld from './HelloWorld';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import ImageSlider from './components/ImageSlider';
 
-// Assurez-vous que l'élément avec l'ID 'react-root' existe dans votre HTML
-const container = document.getElementById('react-root');
-const root = createRoot(container); // Créez la racine
+const App = () => {
+  return (
+    <div>
+      <ImageSlider />
+    </div>
+  );
+};
 
-root.render(
-    <HelloWorld name="Arcadia Zoo" />
-);
+const root = ReactDOM.createRoot(document.getElementById('react-root'));
+root.render(<App />);
