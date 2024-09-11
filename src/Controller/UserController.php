e8f4de8d0137b17ce8d46fcfd1caa75f4060f08e
BrahimZiyani <brahimziyani@live.fr>
2024-09-11 18:03:00 +0200

    UserType, UserController et Security.yaml mis a jour pour la création d'users avec des roles spécifiques

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,18 +9,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
     #[Route('/new', name: 'user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Encodage du mot de passe avec UserPasswordHasherInterface
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user, 
+                $form->get('password')->getData()
+            );
+            $user->setPassword($hashedPassword);
+
+            // Persistance de l'utilisateur dans la base de données
             $entityManager->persist($user);
             $entityManager->flush();
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,18 +6,37 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('nom')
-            ->add('prenom')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,  // Pour permettre plusieurs rôles (array)
+                'expanded' => true,  // Pour afficher comme des cases à cocher
+                'label' => 'Rôles'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
