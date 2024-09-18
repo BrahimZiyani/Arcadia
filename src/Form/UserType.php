8cd5551e6fb206d70c78de403806f163467d4077
BrahimZiyani <brahimziyani@live.fr>
2024-09-18 12:18:10 +0200

    UserType mis à jour

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,15 +10,11 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Security\Core\Security;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        // Récupérer le service Security à partir des options
-        $security = $options['security'];
-
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Email'
@@ -33,26 +29,23 @@ class UserType extends AbstractType
                 'label' => 'Mot de passe'
             ]);
 
-        // Seul un administrateur peut attribuer des rôles
-        if ($security && $security->isGranted('ROLE_ADMIN')) {
-            $builder->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Utilisateur' => 'ROLE_USER',
-                    'Administrateur' => 'ROLE_ADMIN',
-                ],
-                'multiple' => true,  // Pour permettre plusieurs rôles
-                'expanded' => true,  // Affiche des cases à cocher
-                'label' => 'Rôles'
-            ]);
-        }
+        // Si nécessaire, tu peux ajouter une logique métier dans le contrôleur
+        // pour afficher les rôles uniquement si c'est un administrateur.
+        $builder->add('roles', ChoiceType::class, [
+            'choices' => [
+                'Utilisateur' => 'ROLE_USER',
+                'Administrateur' => 'ROLE_ADMIN',
+            ],
+            'multiple' => true,  // Permet plusieurs rôles
+            'expanded' => true,  // Affiche des cases à cocher
+            'label' => 'Rôles'
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-            // Ajouter une option pour passer le service Security
-            'security' => null,
         ]);
     }
 }
