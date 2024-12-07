df6dd52c69e10c9197fa6af71e9d72a075d4eab2
Brahim Ziyani <brahimziyani@live.fr>
2024-12-07 13:15:05 +0100

    Index correction

--- public/index.php
+++ public/index.php
@@ -7,10 +7,11 @@ if (php_sapi_name() === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 }
 
-// Charger uniquement les variables d'environnement si on n'est PAS en production
-if ($_SERVER['APP_ENV'] !== 'prod') {
+// Ne pas charger .env si le fichier n'existe pas ou si APP_ENV est défini sur "prod"
+if ($_SERVER['APP_ENV'] !== 'prod' && file_exists(dirname(__DIR__) . '/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
+
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
