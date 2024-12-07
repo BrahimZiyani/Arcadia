8491c634890b5aeb4eff4da1d492e1b0228cff27
Brahim Ziyani <brahimziyani@live.fr>
2024-12-07 13:41:31 +0100

    debug index.php

--- public/index.php
+++ public/index.php
@@ -3,17 +3,17 @@
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
+$_SERVER['APP_ENV'] = 'prod';
+$_SERVER['APP_DEBUG'] = false;
+
 if (php_sapi_name() === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 }
 
-
 if ($_SERVER['APP_ENV'] !== 'prod') {
-    // Suppression temporaire pour identifier le problème
+    // Décommentez la ligne suivante pour charger les variables d'environnement en mode non-production
     // (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
-
-
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
