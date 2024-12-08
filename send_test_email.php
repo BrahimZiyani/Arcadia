05d1b1cce07412edea48edefa0b4a48d9a3d2a0a
Brahim Ziyani <brahimziyani@live.fr>
2024-12-08 01:02:52 +0100

    send_test_email

---
+++ send_test_email.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+
+// Configure ton DSN (copie directement depuis Heroku ou ton .env)
+$dsn = getenv('MAILER_DSN');
+
+$transport = Transport::fromDsn($dsn);
+$mailer = new Mailer($transport);
+
+$email = (new Email())
+    ->from('arcadia.zooapp@gmail.com') // Adresse validée dans SendGrid
+    ->to('arcadia.zooapp@gmail.com')   // Adresse de destination
+    ->subject('Test Symfony Mailer')
+    ->text('Ceci est un e-mail de test depuis Symfony Mailer.');
+
+try {
+    $mailer->send($email);
+    echo "E-mail envoyé avec succès.\n";
+} catch (\Exception $e) {
+    echo "Erreur : " . $e->getMessage() . "\n";
+}
